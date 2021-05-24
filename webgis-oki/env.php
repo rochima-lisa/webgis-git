12f064ec00c0759aff5e9057347c51a84b30915e
rochima-lisa <84735286+rochima-lisa@users.noreply.github.com>
2021-05-25 00:40:26 +0700

    Add files via upload

---
+++ webgis-oki/env.php
@@ -0,0 +1,17 @@
+<?php
+defined('env') OR exit('Akses langsung ke script ini diblokir');
+
+$setDb['db_host']='127.0.0.1';
+$setDb['db_name']='databasekarhutla';
+$setDb['db_user']='root';
+$setDb['db_password']='';
+
+// folder templates
+$template='templates/Green/';
+
+//session
+$setSession['prefix']='webgis-oki';
+
+//URI
+$setUri['base']='http://localhost/webgis-oki/';
+$setUri['assets']='assets/';
